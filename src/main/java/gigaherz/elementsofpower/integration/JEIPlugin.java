6bc6316e4f2290a7aa8720b7b55f237594340215
David Quintana <gigaherz@gmail.com>
2016-05-06 10:27:52 +0200

    Beginnings of JEI integration (just a skeleton, actual recipes to come later).

---
+++ src/main/java/gigaherz/elementsofpower/integration/JEIPlugin.java
@@ -0,0 +1,25 @@
+package gigaherz.elementsofpower.integration;
+
+import mezz.jei.api.IJeiRuntime;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.INbtIgnoreList;
+import net.minecraft.item.ItemStack;
+
+import javax.annotation.Nonnull;
+import java.util.Arrays;
+
+@mezz.jei.api.JEIPlugin
+public class JEIPlugin implements IModPlugin
+{
+    @Override
+    public void register(@Nonnull IModRegistry registry)
+    {
+        
+    }
+
+    @Override
+    public void onRuntimeAvailable(@Nonnull IJeiRuntime jeiRuntime)
+    {
+    }
+}
