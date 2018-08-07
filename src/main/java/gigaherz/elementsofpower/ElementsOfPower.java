6aba86c96f5aa583895d633b3fb5a7efa85809bd
gigaherz <gigaherz@gmail.com>
2018-08-07 16:44:53 +0200

    This was just a test, never supposed to be commited.

--- src/main/java/gigaherz/elementsofpower/ElementsOfPower.java
+++ src/main/java/gigaherz/elementsofpower/ElementsOfPower.java
@@ -25,17 +25,12 @@ import gigaherz.elementsofpower.spells.blocks.BlockDust;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.*;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.client.renderer.vertex.VertexFormat;
-import net.minecraft.client.renderer.vertex.VertexFormatElement;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -54,17 +49,9 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.oredict.OreDictionary;
 import org.apache.logging.log4j.Logger;
 
-import javax.vecmath.Matrix3d;
-import javax.vecmath.Vector3d;
 import java.io.File;
-import java.nio.ByteBuffer;
 import java.text.DecimalFormat;
 import java.text.Format;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-
-import static org.lwjgl.opengl.GL11.GL_QUADS;
 
 //import gigaherz.elementsofpower.progression.DiscoveryHandler;
 
@@ -380,144 +367,5 @@ public class ElementsOfPower
     public static ResourceLocation location(String location)
     {
         return new ResourceLocation(MODID, location);
-    }
-
-
-    @Mod.EventBusSubscriber(value = Side.CLIENT, modid = MODID)
-    public static class Test
-    {
-        public static final Test INSTANCE = new Test();
-
-        @SubscribeEvent
-        public static void worldLast(RenderWorldLastEvent event)
-        {
-            INSTANCE.render(event.getPartialTicks());
-        }
-
-
-        private static Star[] stars = new Star[4096];
-        static
-        {
-            generateStars();
-        }
-
-        private static void generateStars()
-        {
-            final Random random = new Random(0);
-            final int radius = 100;
-            final double threshold = 0.15;
-
-            for (int i = 0; i < stars.length; i++)
-            {
-                Star star = new Star();
-                double u = 2 * Math.PI * random.nextFloat();
-                double v = Math.acos(1 - 2 * random.nextFloat()) - Math.PI*0.5;
-                double size = (1 + 2 * random.nextFloat()) * .05f;
-
-                // prevents aliasing artifacts, making small stars fainter but not smaller
-                star.alpha = Math.min(size * (1/threshold), 1);
-                if (size < threshold)
-                    size = threshold;
-
-                star.points = new Vector3d[]{
-                        new Vector3d(-size*0.5,-size*0.5,radius),
-                        new Vector3d(-size * 2,0,radius),
-                        new Vector3d(-size*0.5,size*0.5,radius),
-                        new Vector3d(0,0,radius),
-
-                        new Vector3d(-size*0.5,size*0.5,radius),
-                        new Vector3d(0,size*2,radius),
-                        new Vector3d(size*0.5,size*0.5,radius),
-                        new Vector3d(0,0,radius),
-
-                        new Vector3d(size*0.5,size*0.5,radius),
-                        new Vector3d(size*2,0,radius),
-                        new Vector3d(size*0.5,-size*0.5,radius),
-                        new Vector3d(0,0,radius),
-
-                        new Vector3d(size*0.5,-size*0.5,radius),
-                        new Vector3d(0,-size*2,radius),
-                        new Vector3d(-size*0.5,-size*0.5,radius),
-                        new Vector3d(0,0,radius),
-                };
-
-                Matrix3d matrix1 = new Matrix3d();
-                matrix1.rotY(u);
-                Matrix3d matrix2 = new Matrix3d();
-                matrix2.rotX(v);
-                matrix1.mul(matrix2);
-
-                Arrays.stream(star.points).forEach(matrix1::transform);
-
-                stars[i] = star;
-            }
-        }
-
-        private BufferBuilder bufferbuilder;
-        public void render(float partialTicks)
-        {
-            GlStateManager.disableFog();
-            GlStateManager.disableAlpha();
-            GlStateManager.enableBlend();
-            GlStateManager.disableTexture2D();
-            GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-            RenderHelper.disableStandardItemLighting();
-            GlStateManager.depthMask(false);
-            if (bufferbuilder == null)
-            {
-                bufferbuilder = new BufferBuilder(stars.length * 8);
-                bufferbuilder.begin(GL_QUADS, DefaultVertexFormats.POSITION_COLOR);
-                for (Star star : stars)
-                {
-                    for (Vector3d point : star.points)
-                    {
-                        bufferbuilder.pos(point.x, point.y, point.z).color(255, 255, 255, (int) (255 * star.alpha)).endVertex();
-                    }
-                }
-                bufferbuilder.finishDrawing();
-            }
-            draw(bufferbuilder);
-
-            GlStateManager.depthMask(true);
-            GlStateManager.enableTexture2D();
-            GlStateManager.enableAlpha();
-        }
-
-        public void draw(BufferBuilder bufferBuilderIn)
-        {
-            if (bufferBuilderIn.getVertexCount() > 0)
-            {
-                VertexFormat vertexformat = bufferBuilderIn.getVertexFormat();
-                int i = vertexformat.getSize();
-                ByteBuffer bytebuffer = bufferBuilderIn.getByteBuffer();
-                List<VertexFormatElement> list = vertexformat.getElements();
-
-                for (int j = 0; j < list.size(); ++j)
-                {
-                    VertexFormatElement vertexformatelement = list.get(j);
-                    bytebuffer.position(vertexformat.getOffset(j));
-
-                    // moved to VertexFormatElement.preDraw
-                    vertexformatelement.getUsage().preDraw(vertexformat, j, i, bytebuffer);
-                }
-
-                GlStateManager.glDrawArrays(bufferBuilderIn.getDrawMode(), 0, bufferBuilderIn.getVertexCount());
-                int i1 = 0;
-
-                for (int j1 = list.size(); i1 < j1; ++i1)
-                {
-                    VertexFormatElement vertexformatelement1 = list.get(i1);
-
-                    // moved to VertexFormatElement.postDraw
-                    vertexformatelement1.getUsage().postDraw(vertexformat, i1, i, bytebuffer);
-                }
-            }
-        }
-
-        private static class Star
-        {
-            public Vector3d[] points;
-            public double alpha;
-        }
     }
 }
